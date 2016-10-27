dd4af4e9e56021e13318bbe7ae810582224f824a
Jaime Lopez <loama18@gmail.com>
2016-10-27 01:11:47 -0500

    create website app and first view

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
+import website.views
+
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', website.views.homePage, name='website_home'),
+    url(r'^admin/', include(admin.site.urls))
 ]
