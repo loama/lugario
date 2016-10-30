f607f9f489556dafa9e942c9432d5fba11843d30
Jaime Lopez <loama18@gmail.com>
2016-10-29 23:55:36 -0500

    commit

--- gettingstarted/static/js/init.js
+++ gettingstarted/static/js/init.js
@@ -10,16 +10,58 @@
 function initMap() {
   var w = window.innerWidth;
   disableDefaultUIMaps = true;
-  if (w > 992) {
+  if (w < 992) {
   	disableDefaultUIMaps = false;
   }
   console.log(disableDefaultUIMaps);
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 19.422893, lng: -99.174298},
     zoom: 13,
-    styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#ffdfa6"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#b52127"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#c5531b"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#74001b"},{"lightness":-10}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#da3c3c"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#74001b"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#da3c3c"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#990c19"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#74001b"},{"lightness":-8}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#6a0d10"},{"visibility":"on"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"color":"#ffdfa6"},{"weight":0.4}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]}],
-    disableDefaultUI: disableDefaultUIMaps
+    styles: [{"featureType":"all","elementType":"all","stylers":[{"hue":"#ff2600"},{"saturation":"100"},{"lightness":"-39"},{"gamma":"0.70"}]}],
+    zoomControl: disableDefaultUIMaps,
+  	mapTypeControl: false,
+  	scaleControl: true,
+  	streetViewControl: false,
+  	rotateControl: false
   });
+
+  var settings = {
+	  "async": true,
+	  "crossDomain": true,
+	  "url": "https://lugarapi.herokuapp.com/website_properties",
+	  "method": "GET",
+	  "headers": {
+	    "cache-control": "no-cache",
+	  }
+	}
+
+	$.ajax(settings).done(function (response) {
+	  for (i=0; i < response.length; i++) {
+	  	var image = 'static/img/map_marker.png';
+	  	var location = JSON.stringify(response[i].location);
+		var latitude = location.split(",")[10].split(":")[1];
+		//console.log(latitude);
+		var longitude = location.split(",")[11].split(":")[1];
+		//console.log(longitude);
+		if(isNaN(latitude)) {}
+			else {
+				if (isNaN(longitude)) {}
+					else {
+						console.log(latitude);
+						eval('var marker' + i + ' = new google.maps.Marker({' +
+						    'position: {lat: ' + latitude + ', lng: ' + longitude + '},' +
+						    'title: "' + response[i].id + '",' +
+						    'map: map,' +
+						    'icon: "' + image + '"});' + 
+						    'marker' + i + '.addListener("click", function() {' +
+							    'console.log(marker' + i + '.getTitle())' +
+							'});');
+					}
+			}
+		
+	  }
+	  console.log(i);
+	});
 
   // places autocomplete
 
@@ -34,6 +76,59 @@ function initMap() {
       window.alert("Autocomplete's returned place contains no geometry");
       return;
  	}
+
+ 	if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);  // Why 17? Because it looks good.
+    }
+
  	console.log(place);
-  }); 	
-};  
+  });
+
+  var image = 'static/img/map_marker.png';
+  var marker = new google.maps.Marker({
+    position: {lat: 19.4220, lng: -99.174},
+    map: map,
+    icon: image
+  });
+};
+
+$("#change-view-list").click( function() {
+	$("#change-view-map").css('color', 'white');
+	$(".view-indicator").css('left', '3px');
+	$(this).css('color', '#777');
+	$('#list-view').css('display', 'block');
+	$('#list-view').animateCss('bounceInLeft', 'in');
+});
+
+$("#change-view-map").click( function() {
+	$("#change-view-list").css('color', 'white');
+	$(".view-indicator").css('left', '49px');
+	$(this).css('color', '#777');
+	$('#list-view').animateCss('bounceOutLeft', 'out');
+});
+
+$('#close-filter-view').click( function() {
+	$('#filter-view').animateCss('bounceOutUp', 'out');
+});
+
+$('#filter-results').click( function() {
+	$('#filter-view').animateCss('bounceInDown', 'in');
+});
+
+
+$.fn.extend({
+    animateCss: function (animationName, type) {
+    	$(this).css('display', 'block');
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+            if (type == 'out') {
+            	$(this).css('display', 'none');
+            	console.log('out');
+            }
+        });
+    }
+});
--- website/models.py
+++ website/models.py
@@ -6,4 +6,21 @@ from django.db import models
 
 class users(models.Model):
 	name = models.CharField(max_length=200)
+
+class properties(models.Model):
+	meli_id = models.CharField(max_length=500)
+	meli_link = models.CharField(max_length=500)
+	title = models.CharField(max_length=500)
+	price = models.FloatField()
+	currency_id = models.CharField(max_length=200)
+	pictures = models.CharField(max_length=10000)
+	location = models.CharField(max_length=10000)
+	publisher_name = models.CharField(max_length=500, null=True)
+	publisher_phone = models.CharField(max_length=500, null=True)
+	publisher_email = models.CharField(max_length=500, null=True)
+	publisher_link = models.CharField(max_length=500, null=True)
+	property_type = models.CharField(max_length=500)
+
+
+				
 		
